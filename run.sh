272bb93f4479bc7e430573c6a2b093ee797879aa
turanegaku <turanegaku@gmail.com>
2015-12-05 02:47:12 +0900

    compile only file changed

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 name=`ls -t *.cpp | head -n 1`
-g++ -std=c++11 $name
+md5=`md5 -q $name`
+if [[ -e .prebuild ]]; then
+  pmd5=`cat .prebuild`
+fi
+
+if [[ $pmd5 != $md5 ]]; then
+  g++ -std=c++11 $name
+  echo $md5 > .prebuild
+fi
 if [ $? -eq 0 ];then
   echo "run $name"
   ./a.out
